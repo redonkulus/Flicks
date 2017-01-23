9e4196301df983abffeb3e4d072110bf4c7c3a11
sbertal <sbertal@yahoo-inc.com>
2017-01-23 10:33:00 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by Seth Bertalotto on 1/23/17.
+//  Copyright © 2017 Seth Bertalotto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
